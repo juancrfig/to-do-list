5c6aea01f19bf3dcd6e555aacc4c251548cbae81
juancrfig <juancrfig@gmail.com>
2025-01-08 21:08:05 -0500

    Arreglar bug relacionado con agregar varios eventos sobre el mismo objecto

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
 const addTaskButton = document.querySelector('#addTaskButton');
 const newTaskInput = document.querySelector('input');
-let task = '';
 const boxUl = document.querySelector('ul');
-
 
 addTaskButton.addEventListener('click', () => {
     addTask();
+});
 
-    if (boxUl.childNodes.length > 0) {
+// Using event delegation instead of individual listeners
+boxUl.addEventListener('click', (event) => {
+    const target = event.target;
+    
+    // Handle click on the icon inside the button
+    if (target.tagName === 'I') {
+        const button = target.parentElement;
+        const li = button.parentElement;
         
-        const trashButtons = document.querySelectorAll('#trashButton');
-        const checkButtons = document.querySelectorAll('#checkButton');
-
-        trashButtons.forEach((button) => {
-            
-            button.addEventListener('click', () => {
-                button.parentElement.remove()
-            })
-        })
-
-        checkButtons.forEach((button) => {
-
-            button.addEventListener('click', () => {
-                button.parentElement.classList.toggle('checked')
-            })
-        })
-
+        if (button.id === 'checkButton') {
+            li.classList.toggle('checked');
+        } else if (button.id === 'trashButton') {
+            li.remove();
+        }
     }
-})
+    
+    // Handle click directly on the button
+    if (target.tagName === 'BUTTON') {
+        const li = target.parentElement;
+        
+        if (target.id === 'checkButton') {
+            li.classList.toggle('checked');
+        } else if (target.id === 'trashButton') {
+            li.remove();
+        }
+    }
+});
 
 newTaskInput.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
         event.preventDefault();
         addTaskButton.click();
     }
-})
-
+});
 
 function addTask() {
-
     if (newTaskInput.value === '') {       
         alert('Ingresa una tarea!')
     } else {
-        task = newTaskInput.value;
+        const task = newTaskInput.value;
         newTaskInput.value = '';
         let newTask = document.createElement('li');
         newTask.innerHTML = `
         ${task} <button id="checkButton"><i class="fa-solid fa-check"></i>
-        </button><button id="trashButton"><i class="fa-solid fa-trash"></i></button> `;
+        </button><button id="trashButton"><i class="fa-solid fa-trash"></i></button>`;
         boxUl.appendChild(newTask);
     }
 }
-
-
