3a416d61ecf2f4a2179eeb73cab74140b40b77fd
juancrfig <juancrfig@gmail.com>
2025-01-30 14:36:47 -0500

    Add calendar

--- src/todoApp.js
+++ src/todoApp.js
@@ -1,13 +1,17 @@
+import "../node_modules/cally/dist/cally.js";
+
 const ulProjectsElm = document.querySelector('#projects-ul');
 const newProjectBtn = document.querySelector('#new-project-btn');
-
+const calendarDateEl = document.querySelector('calendar-date');
+const createProjectBtn = document.querySelector('#create-project-btn');
+console.log(createProjectBtn)
 // Array to store all the user's projects
 const userProjects = [];
 
 document.addEventListener('click', (e) => {
     switch (e) {
-        case newProjectBtn:
-            newProjectModal.showModal();
+        case createProjectBtn:
+            console.log('gotcha!')
             break;
         default:
             console.log('C H A M O');
@@ -42,4 +46,5 @@ class Task {
     }
 }
 
-const novaProject = new Project('nova', 'My first project for the todo app', '01/03/2025');
+
+// const novaProject = new Project('nova', 'My first project for the todo app', '01/03/2025');
